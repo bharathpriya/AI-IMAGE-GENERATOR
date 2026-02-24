c0c3e1f90c4279236b9efa1a649921ff41b31775
bharapriya <rathbharath15@gmail.com>
2026-02-24 15:43:27 +0530

    Fix case-sensitive import of Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,26 +5,26 @@ import Home from './pages/Home'
 import Buycredit from './pages/Buycredit'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Login from './components/Login'
 import { AppContext } from './context/AppContext'
 
 const App = () => {
 
-    const {showLogin} = useContext(AppContext)
+  const { showLogin } = useContext(AppContext)
 
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
 
-      <Navbar/>
-      {showLogin && <Login/>}
+      <Navbar />
+      {showLogin && <Login />}
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='result' element={<Result/>}/>
-        <Route path='buycredit' element={<Buycredit/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='result' element={<Result />} />
+        <Route path='buycredit' element={<Buycredit />} />
       </Routes>
-      <Footer/>
-     
+      <Footer />
+
     </div>
   )
 }
